8858485da0240757a94cbda7cb7dd5c58d0779e3
auron77777 <auron77777@gmail.com>
2025-03-22 00:14:48 +0300

    1 ver.

--- app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
@@ -1,21 +1,316 @@
 package com.example.androidpracticumcustomview
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val buttonXml = findViewById<Button>(R.id.xmlActivity)
-        val buttonCompose = findViewById<Button>(R.id.composeActivity)
-        buttonXml.setOnClickListener {
-            startActivity(Intent(this, XmlActivity::class.java))
-        }
-        buttonCompose.setOnClickListener {
-            startActivity(Intent(this, ComposeScreen::class.java))
+        setContent {
+            BakeryApp()
         }
     }
 }
+
+
+@Serializable
+data class BakeryItem(val name: String, val price: Double, val imageRes: Int)
+@Serializable
+data class Cart(val items: Map<String, Int>)
+
+
+fun Map<BakeryItem, Int>.toJson(): String {
+    val items = this.mapKeys { it.key.name }
+    val cart = Cart(items)
+    return Json.encodeToString(cart)
+}
+
+
+class BakeryViewModel : ViewModel() {
+    var cart by mutableStateOf<Map<BakeryItem, Int>>(emptyMap())
+        private set
+
+    var totalPrice by mutableStateOf(0.0)
+        private set
+
+
+    fun addToCart(item: BakeryItem) {
+        val updatedCart = cart.toMutableMap()
+        updatedCart[item] = (updatedCart[item] ?: 0) + 1
+        cart = updatedCart
+        totalPrice += item.price
+    }
+
+
+}
+
+@Composable
+fun BakeryApp() {
+    val navController = rememberNavController()
+    val bakeryViewModel: BakeryViewModel = viewModel()
+
+    NavHost(navController = navController, startDestination = "start_page") {
+        composable("start_page") {
+            StartPage(navController = navController)
+        }
+        composable("bakery_order") {
+            ComposeScreen(navController = navController, bakeryViewModel = bakeryViewModel)
+        }
+
+
+        composable(
+            "item_description/{itemName}/{itemPrice}/{imageRes}",
+            arguments = listOf(
+                navArgument("itemName") { type = NavType.StringType },
+                navArgument("itemPrice") { type = NavType.StringType },
+                navArgument("imageRes") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val itemName = backStackEntry.arguments?.getString("itemName") ?: ""
+            val itemPriceString = backStackEntry.arguments?.getString("itemPrice") ?: "0.0"
+            val imageRes = backStackEntry.arguments?.getInt("imageRes") ?: 0
+
+
+            val itemPrice = itemPriceString.toDouble()
+
+            val bakeryItem = BakeryItem(itemName, itemPrice, imageRes)
+            ItemDescriptionScreen(navController = navController, bakeryItem = bakeryItem)
+        }
+    }
+}
+
+
+
+@Composable
+fun BakeryItemRow(item: BakeryItem, onAddToCart: () -> Unit, navController: NavController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        val image: Painter = painterResource(id = item.imageRes)
+        Image(
+            painter = image,
+            contentDescription = item.name,
+            modifier = Modifier
+                .size(40.dp)
+                .padding(end = 16.dp)
+                .clickable {
+                    navController.navigate("item_description/${item.name}/${item.price}/${item.imageRes}")
+                }
+        )
+
+        Text(
+            text = item.name,
+            color = Color.White,
+            modifier = Modifier
+                .clickable {
+                    navController.navigate("item_description/${item.name}/${item.price}/${item.imageRes}")
+                }
+                .padding(end = 8.dp)
+        )
+
+        Text(
+            text = "${item.price} rub.",
+            color = Color.White
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+
+        Button(
+            onClick = onAddToCart,
+            modifier = Modifier.height(40.dp)
+        ) {
+            Text("add", color = Color.White)
+        }
+    }
+}
+
+
+
+@Composable
+fun ItemDescriptionScreen(navController: NavController, bakeryItem: BakeryItem) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+            }
+        }
+
+        Text(
+            text = bakeryItem.name,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Image(
+            painter = painterResource(id = bakeryItem.imageRes),
+            contentDescription = bakeryItem.name,
+            modifier = Modifier.size(200.dp).padding(bottom = 16.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = "Price: ${"%.2f".format(bakeryItem.price)} rub.",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Button(
+            onClick = { },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Add to Cart")
+        }
+    }
+}
+
+
+@Composable
+fun StartPage(navController: NavController) {
+    val context = LocalContext.current
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+    val rotationAngle = if (isLandscape) 90f else 0f
+    val backgroundImage: Painter = painterResource(id = R.drawable.start_page)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectHorizontalDragGestures { _, dragAmount ->
+                    if (dragAmount < 0) {
+                        navController.navigate("backery_order")
+                    } else if (dragAmount > 0) {
+                        navController.navigate("val intent = Intent(context, BakeryOrderActivity::class.java)\n" +
+                                "                    context.startActivity(intent)")
+                    }
+                }
+            }
+    ) {
+
+        Image(
+            painter = backgroundImage,
+            contentDescription = "Start Page Background",
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(
+                    rotationZ = rotationAngle,
+                    transformOrigin = TransformOrigin.Center
+                ),
+            contentScale = ContentScale.FillBounds
+        )
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "greetings my friend",
+                style = MaterialTheme.typography.headlineLarge.copy(color = Color.Yellow),
+                modifier = Modifier.padding(bottom = 100.dp)
+            )
+
+
+            Spacer(modifier = Modifier.height(300.dp))
+
+
+            Button(
+                onClick = { navController.navigate("bakery_order") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .shadow(2.dp)
+            ) {
+                Text("make order compose")
+            }
+
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Button(
+                onClick = {
+                    val intent = Intent(context, XmlActivity::class.java)
+                    context.startActivity(intent)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .shadow(2.dp)
+            ) {
+                Text("make order xml")
+            }
+
+        }
+    }
+
+}
+
--- app/src/main/java/com/example/androidpracticumcustomview/XmlActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/XmlActivity.kt
@@ -1,39 +1,98 @@
 package com.example.androidpracticumcustomview
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
-import androidx.activity.ComponentActivity
-import com.example.androidpracticumcustomview.ui.theme.CustomContainer
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
+class XmlActivity : AppCompatActivity() {
 
-class XmlActivity : ComponentActivity() {
+    private lateinit var bakeryItems: List<BakeryItem>
+    private lateinit var bakeryAdapter: BakeryItemAdapter
+    private lateinit var totalPriceText: TextView
+    private var cartItems: MutableList<BakeryItem> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startXmlPracticum()
+        setContentView(R.layout.activity_bakery_order)
+
+
+        val backButton: ImageButton = findViewById(R.id.back_button)
+        totalPriceText = findViewById(R.id.total_price_text)
+
+
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+
+        bakeryItems = listOf(
+            BakeryItem("Baguette", 40.0, R.drawable.baget),
+            BakeryItem("Croissant", 50.0, R.drawable.kruassan)
+        )
+
+        bakeryAdapter = BakeryItemAdapter(bakeryItems) { bakeryItem ->
+            addToCart(bakeryItem)
+        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.bakery_items_recycler)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = bakeryAdapter
+
+        updateTotalPrice()
+
+        animateView(backButton, 300)
+        animateView(findViewById(R.id.title), 500)
+        animateView(recyclerView, 700)
+        animateView(totalPriceText, 900)
     }
 
-    private fun startXmlPracticum() {
-        val customContainer = CustomContainer(this)
-        setContentView(customContainer)
-        customContainer.setOnClickListener {
-            finish()
-        }
+    private fun addToCart(bakeryItem: BakeryItem) {
+        cartItems.add(bakeryItem)
+        updateTotalPrice()
+    }
 
-        val firstView = TextView(this).apply {
-            // TODO
-            // ...
-        }
+    private fun updateTotalPrice() {
+        val totalPrice = cartItems.sumByDouble { it.price }
 
-        val secondView = TextView(this).apply {
-            // TODO
-            // ...
-        }
+        val totalPriceFloat = totalPrice.toFloat()
 
-        // Добавление второго элемента через некоторое время (например, по задержке)
-        Handler(Looper.getMainLooper()).postDelayed({
-            customContainer.addView(secondView)
-        }, 2000)
+
+        val animator = ValueAnimator.ofFloat(totalPriceFloat - (totalPriceFloat / 2), totalPriceFloat)
+        animator.duration = 1000  //
+        animator.addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue as Float
+            totalPriceText.text = "Total: %.2f rub".format(animatedValue)
+        }
+        animator.start()
+    }
+
+    private fun animateView(view: View, delay: Long) {
+
+        val scaleXAnimator = ObjectAnimator.ofFloat(view, View.SCALE_X, 1f)
+        val scaleYAnimator = ObjectAnimator.ofFloat(view, View.SCALE_Y, 1f)
+
+        val translationXAnimator = ObjectAnimator.ofFloat(view, View.TRANSLATION_X, 0f)
+        val translationYAnimator = ObjectAnimator.ofFloat(view, View.TRANSLATION_Y, 0f)
+
+        scaleXAnimator.startDelay = delay
+        scaleYAnimator.startDelay = delay
+        translationXAnimator.startDelay = delay
+        translationYAnimator.startDelay = delay
+
+        scaleXAnimator.duration = 5000
+        scaleYAnimator.duration = 5000
+        translationXAnimator.duration = 5000
+        translationYAnimator.duration = 5000
+
+        scaleXAnimator.start()
+        scaleYAnimator.start()
+        translationXAnimator.start()
+        translationYAnimator.start()
     }
 }
